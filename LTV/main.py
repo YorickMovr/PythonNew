14426ef7fdcd2c5d553305a407d6f2f1172ce50b
YorickMovr <yoricksamovar@gmail.com>
2019-10-18 16:27:43 +0300

    d

--- LTV/main.py
+++ LTV/main.py
@@ -19,10 +19,52 @@ fid.close()
 # делим файл тут
 # не правильно работает 
 
-csvfile = open('c:/Users/Ярослав/Documents/GitHub/PythonNew/LTV/shuffled_smth.csv', 'r').readlines()
+# csvfile = open('c:/Users/Ярослав/Documents/GitHub/PythonNew/LTV/shuffled_smth.csv', 'r').readlines()
+# print(len(csvfile))
+# print(csvfile[1][0])
+# -------------------------------------
+# row_sum = math.floor(len(csvfile) *0.7)
+# print(row_sum)
+# filename = 1
+# for i in range(len(csvfile)):
+#     if i< row_sum:
+#         open(str(filename) + '.csv', 'w+').writelines(csvfile[i:i+row_sum])
+        # filename +
+# ----------------------------------------
+# for i in range(len(csvfile)):
+#      if i % 100 == 0:
+#          open(str(filename) + '.csv', 'w+').writelines(csvfile[i:4])
+#          filename += 1
+# ----------------------------------------------
+import os
 
-filename = 1
-for i in range(len(csvfile)):
-     if i % 10 == 0:
-         open(str(filename) + '.csv', 'w+').writelines(csvfile[i:i+1000])
-         filename += 1
+def split(filehandler, delimiter=',', row_limit=1000,
+          output_name_template='output_%s.csv', output_path='.', keep_headers=True):
+    import csv
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    current_piece = 1
+    current_out_path = os.path.join(
+        output_path,
+        output_name_template % current_piece
+    )
+    current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    current_limit = row_limit
+    if keep_headers:
+        headers = reader.next()
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+                output_path,
+                output_name_template % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+
+csvfile = open('c:/Users/Ярослав/Documents/GitHub/PythonNew/LTV/shuffled_smth.csv', 'r').readlines()
+countOfRowsFor1file = math.floor(len(csvfile) * 0.7)
+split(csvfile, countOfRowsFor1file)
